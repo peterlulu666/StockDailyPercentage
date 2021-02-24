0a6ac4f9724d6422268e4984fb707e379a5e239d
vpslinuxinstall <shoparoundwyz@gmail.com>
2021-02-24 16:15:07 -0600

    commit

--- stock.py
+++ stock.py
@@ -2,10 +2,10 @@ import yfinance
 import csv
 import os
 
-prev_start_date = "2021-02-22"
-prev_start_end_date = "2021-02-23"
-start_date = "2021-02-23"
-end_date = "2021-02-24"
+prev_start_date = "2021-02-23"
+prev_end_date = "2021-02-24"
+start_date = "2021-02-24"
+end_date = "2021-02-25"
 with open('stock_list.csv', newline='') as f_stock_list:
     reader_stock_list = csv.reader(f_stock_list)
     stock_name_list = list(reader_stock_list)
@@ -14,8 +14,8 @@ csv_fields = ["Stock name", "Percentage change"]
 csv_rows = []
 
 
-def stock_percentage(prev_start_date, prev_start_end_date, start_date, end_date, stock_name):
-    yfinance.download(stock_name, start=prev_start_date, end=prev_start_end_date).to_csv("prev" + stock_name + ".csv")
+def stock_percentage(prev_start_date, prev_end_date, start_date, end_date, stock_name):
+    yfinance.download(stock_name, start=prev_start_date, end=prev_end_date).to_csv("prev" + stock_name + ".csv")
     yfinance.download(stock_name, start=start_date, end=end_date).to_csv(stock_name + ".csv")
     with open("prev" + stock_name + ".csv", newline='') as f:
         reader = csv.reader(f)
@@ -34,7 +34,7 @@ def stock_percentage(prev_start_date, prev_start_end_date, start_date, end_date,
 
 def main():
     for stock_name in stock_name_list:
-        stock_percentage(prev_start_date, prev_start_end_date, start_date, end_date, str(stock_name[0]))
+        stock_percentage(prev_start_date, prev_end_date, start_date, end_date, str(stock_name[0]))
 
     with open("Stock.csv", 'w') as csv_file:
         # creating a csv writer object
